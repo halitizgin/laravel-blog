e8d281e9fefb563ffea3b90fe289d8a64ecd2c73
Halit IZGIN <halit_izgin@outlook.com.tr>
2023-01-19 03:47:03 +0300

    Laravel Validation Hataları ve Stillendirme

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,13 +4,19 @@
     <div class="wrapper">
         <form class="form" action="{{ route('login.auth') }}" method="POST">
             @csrf
-            <div class="form-outline mb-4">
+            <div class="form-outline mb-4 @error('email') invalid @enderror">
                 <label class="form-label" for="email">Email</label>
                 <input id="email" name="email" class="form-control" required type="text" placeholder="E-Mail" />
+                @error('email')
+                    <small>{{ $message }}</small>
+                @enderror
             </div>
-            <div class="form-outline mb-4">
+            <div class="form-outline mb-4 @error('password') invalid @enderror">
                 <label name="password" class="form-label" for="password">Password</label>
                 <input id="password" name="password" class="form-control" required type="password" placeholder="Password" />
+                @error('password')
+                    <small>{{ $message }}</small>
+                @enderror
             </div>
 
             <button class="btn btn-primary btn-block" type="submit">Login</button>
