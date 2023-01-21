af13b099d1e69542b33f69063daf531709314134
Halit IZGIN <halit_izgin@outlook.com.tr>
2023-01-21 18:44:42 +0300

    Post Component'i Oluşturma ve Kullanma

---
+++ resources/views/components/post.blade.php
@@ -0,0 +1,24 @@
+<div class="blog-entry ftco-animate">
+    <div class="text text-2 pt-2 mt-3">
+        <span class="category mb-3 d-block"><a href="#">{{ $post->category->name }}</a></span>
+        <h3 class="mb-4"><a href="#">{{ $post->title }}</a></h3>
+        <p class="mb-4">{{ $post->short_content }}</p>
+        <div class="author mb-4 d-flex align-items-center">
+            <div class="ml-3 info">
+                <span>Written by</span>
+                <h3><a href="#">{{ $post->user->name }}</a></h3>
+                <small><span>{{ $post->created_at }}</span></small>
+            </div>
+        </div>
+        <div class="meta-wrap align-items-center">
+            <div class="half order-md-last">
+                <p class="meta">
+                    <span><i class="icon-comment"></i>{{ $post->comments->count() }}</span>
+                </p>
+            </div>
+            <div class="half">
+                <p><a href="#" class="btn py-2">Continue Reading <span class="ion-ios-arrow-forward"></span></a></p>
+            </div>
+        </div>
+    </div>
+</div>
